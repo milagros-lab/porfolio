539744d7b7cf9acff1751bcd0be3bc714e9e196b
Mila <85306650+milagros-lab@users.noreply.github.com>
2023-04-19 11:08:25 +0200

    Update CardMe.jsx

--- src/components/CardMe.jsx
+++ src/components/CardMe.jsx
@@ -1,9 +1,7 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import perfil from "../media/perfilfoto.jpg";
 
 
 export default function CardMe() {
