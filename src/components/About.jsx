eebf954d8a1b51f15241245c5fbff3c773c1bbfc
mila <elichaur@gmail.com>
2023-06-06 22:06:56 +0200

    cambios en sobre mí

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
         </Typography>
         <Typography variant="body2" color="white" padding={"16px"}>
           Me motiva seguir aprendiendo y creciendo como desarrrolladora, y
-          llegar a convertirme en un activo importe dentro de un equipo de
+          llegar a convertirme en un activo importante dentro de un equipo de
           trabajo. Se me da muy bien el trabajo en equipo, soy perseverante y
           empática, y tengo gran capacidad organizativa.
         </Typography>
