961af20aeadb85593c62cf4a80615409521e66af
mila <elichaur@gmail.com>
2024-03-13 17:24:28 +0100

    cambios cv

--- src/constants/project.js
+++ src/constants/project.js
@@ -1,7 +1,9 @@
 import tiendaOnline from "..//assets/img/tienda-online.png";
 import inmohouse from "..//assets/img/inmohouse.png";
 import meals from "..//assets/img/meals.png";
-import industry from "..//assets/img/industry.png"
+import industry from "..//assets/img/industry.png";
+import movie from "..//assets/img/movie.png";
+
 
 export const projects = [
   {
@@ -17,6 +19,18 @@ export const projects = [
     codigo: "https://github.com/milagros-lab/InmoHouse"
   },
   {
+    title: "appMovie",
+    description:
+      "App creada con React, redux, mui, firebase para la autenticación de usuario. Se trata de una plataforma digital para reproducir peliculas. Se conecta a la API The Movie Database (TMDB) para generarnos un listado de películas.",
+    image: movie,
+    tags: ["Html", "React", "Material UI", "Firebase"],
+    source: "",
+    liveVersion: "",
+    id: 1,
+    enlace : "https://appmovie-lab.netlify.app/",
+    codigo: "https://github.com/milagros-lab/appMovie"
+  },
+  {
     title: "Tienda online",
     description:
       "Aplicación web interactiva realizada utilizando tecnologías modernas como React para la interfaz de usuario, Firebase para la gestión de datos y la autenticación, y Material-UI (MUI) para un diseño atractivo y altamente funcional.",
@@ -24,8 +38,8 @@ export const projects = [
     tags: ["React", "JavaScript", "Firebase", "MUI"],
     source: "",
     liveVersion: "",
-    id: 1,
-    enlace : "https://tiendaonlinemila.netlify.app/",
+    id: 2,
+    enlace : "https://easychooping.netlify.app/",
     codigo: "https://github.com/milagros-lab/tienda-online"
   },
   {
@@ -36,9 +50,9 @@ export const projects = [
     tags: ["React", "JavaScript", "Axios"],
     source: "",
     liveVersion: "",
-    id: 2,
-    enlace : "https://meals-app.milagroschamorr.repl.co/",
-    codigo: "https://replit.com/@milagroschamorr/Meals-App"
+    id: 3,
+    enlace : "https://meals-lab.netlify.app/",
+    codigo: "https://github.com/milagros-lab/proyectos-replit"
   },
   {
     title: "Web industry",
@@ -48,8 +62,21 @@ export const projects = [
     tags: ["Html", "Saas", "Bootstrap"],
     source: "",
     liveVersion: "",
-    id: 2,
+    id: 4,
     enlace : "https://zesty-chebakia-c53ed2.netlify.app/",
     codigo: "https://github.com/milagros-lab/industry"
   },
+  {
+    title: "Web fashion",
+    description:
+      "Maquetacion de web con css, imagenes responsive.",
+    image: industry,
+    tags: ["Html", "Saas", "Bootstrap"],
+    source: "",
+    liveVersion: "",
+    id: 4,
+    enlace : "https://zesty-chebakia-c53ed2.netlify.app/",
+    codigo: "https://github.com/milagros-lab/industry"
+  },
+ 
 ];
